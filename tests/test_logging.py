989c539b26fdd153a0d2066c25b7fbc4cde3655e
Tapas <tapaswinck@gmail.com>
2026-07-15 20:46:55 +0530

    Add logger implementation

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,10 +1,16 @@
+import pytest
+
 from exomanifold.utils.logging import get_logger
 
-def test_logger():
-    logger = get_logger("test")
+@pytest.fixture
+def logger():
+    return get_logger("test_logger")
 
-    logger.info("Hello ExoManifold")
-    
-    assert logger.name == "test"
+def test_logger_name(logger):
+    assert logger.name == "test_logger"
 
-    
+def test_logger_has_handlers(logger):
+    assert len(logger.handlers) == 2
+
+def test_log_file_exists(logger):
+    logger.info("Testing logger")
